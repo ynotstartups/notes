07d64ce212f54ec6f6ff1367277d5219da22282f
ynotstartups <ynotstartups@gmail.com>
2021-12-29 10:39:33 +0000

    install venv and upgrade pip

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+### Check if a directory does not exist ###
+if [ ! -d './venv' ]
+then
+    python3.10 -m venv .venv
+fi
+
 sudo apt-get update -y
 sudo apt-get install -y exiv2
 
 source .venv/bin/activate
+
+pip install --upgrade pip
 
 pip install -r requirements.txt
 
