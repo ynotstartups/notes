52a669603e54c0913d5f9cc936680ef7ac8c0dae
ynotstartups <ynotstartups@gmail.com>
2023-04-03 08:55:24 +0100

    add pre-commit install & install to global python

--- install.sh
+++ install.sh
@@ -3,19 +3,10 @@
 # remember to git clone git@github.com:ynotstartups/ynotstartups.github.io.git
 # if `~/Documents/ynotstartups.github.io` doesn't exist
 
-# install venv, ubuntu doesn't come it
-sudo apt install python3.10-venv
-
-### Check if a directory does not exist ###
-if [ ! -d './venv' ]
-then
-    python -m venv .venv
-fi
-
-source .venv/bin/activate
-
 pip install --upgrade pip
 
 pip install -r requirements.txt
+
+pre-commit install
 
 ln -f pre-push ./.git/hooks/pre-push
